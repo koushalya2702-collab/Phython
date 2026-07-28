9e79e5ee0560a8f66d4597df25f9b48f60921b67
koushalya2702-collab <koushalya2702@gmail.com>
2026-07-28 16:11:31 +0530

    add 1.py

---
+++ 1.py
@@ -0,0 +1,53 @@
+#Type Checking:
+x=5
+print(type(x))
+
+x="koushalya"
+print(type(x))
+
+# Type Conversion
+x=10
+y=float(x)
+print(y)
+
+y="70"
+x=int(y)
+z=float(x)
+print(z)
+
+#Arithmetic Operators
+a=9
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+#Assigning Values to Multiple Variables
+x,y,z=4,5,6
+print(x)
+print(y)
+print(z)
+
+#Swap Two Variables
+a=2
+b=9
+a,b=b,a
+print(f"value of a is:",a)
+print(f"value of b is:",b)
+
+##Swap Two Variables using third variable
+a=9
+b=6
+print(f"value of a:",a)
+print(f"value of b:",b)
+
+temp=a
+a=b
+b=temp
+
+print(f"value of a:",a)
+print(f"value of b:",b)
+
