8f66f6d7d9f4c80c4fab2243c833d49b8d38f59a
koushalya2702-collab <koushalya2702@gmail.com>
2026-08-22 22:48:57 +0530

    add calculation.py

---
+++ calculation.py
@@ -0,0 +1,14 @@
+
+'''
+Create Your Module
+Write a file calculations.py with functions: add(), subtract(), multiply()
+Use it in another file by importing and testing all functions.
+'''
+
+
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
